3dbbf0c1a22902459d85bc7d9973028209bc36af
Peter Kleiweg <kleiweg@ziggo.nl>
2026-06-02 11:53:42 +0200

    update tokenizer in Alpino

--- nobr/libtok_no_breaks.c
+++ nobr/libtok_no_breaks.c
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:fc9556d2455567a9f91f50241c8f624e46d9240024047bd74970ff0c9eee6d98
-size 46582252
+oid sha256:d54c3a6e5a039c245d4c80ea7edff24ba89b88f1d24d0c9ca40fc0e8ab90b013
+size 47645682
